37e92cf8e0d1f61e7bdd60de78f8611a16881151
ROSHAN-456 <roshanmagesh07@gmail.com>
2026-05-13 15:17:04 +0530

    Revert "TRIAL 1"
    This reverts commit 43eb3fc20c12f9226525afaa90eea968d7d4de80.

--- imperium-app/gh-pages-app/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base "./" => works at https://<user>.github.io/<repo>/ without hardcoding the repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-});
